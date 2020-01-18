0edef555a3b616239a6e0c6d3d66b39e893fb853
hadi norouzi <hadinorouzi78@gmail.com>
2020-01-18 23:58:29 +0330

    - fix icons size - change reset button to cupertinoButton - add showDialog for winners

--- lib/Board.dart
+++ lib/Board.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:TicTacToe/utils/util.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'model/BoardValues.dart';
@@ -10,84 +11,82 @@ class Board extends StatelessWidget {
   final dynamic parentClass;
   var scoreBoardValues;
   var boardValues;
+  var context;
 
   Board(this.parentClass);
   @override
   Widget build(BuildContext context) {
     scoreBoardValues = Provider.of<ScoreBoardValues>(context);
     boardValues = Provider.of<BoardValues>(context);
+    this.context = context;
 
-    return Card(
-      elevation: 100,
-      color: Colors.red,
-      child: Container(
-        color: Color(0xFF3E7D91),
-        child: boardValues.won
-            ? Opacity(
-                opacity: boardValues.won ? 1 : 0.0,
-                child: FlareActor("asset/celebration.flr",
-                    fit: BoxFit.contain,
-                    animation: boardValues.won ? "Untitled" : null,
-                    alignment: Alignment.center,
-                    controller: parentClass),
-              )
-            : Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 1.2,
+    return Container(
+      color: Color(0xFF3E7D91),
+      child: boardValues.won
+          ? Opacity(
+              opacity: boardValues.won ? 1 : 0.0,
+              child: FlareActor("asset/celebration.flr",
+                  fit: BoxFit.contain,
+                  animation: boardValues.won ? "Untitled" : null,
                   alignment: Alignment.center,
-                  height: MediaQuery.of(context).size.width / 1.2,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Expanded(
-                        child: Row(
-                          children: <Widget>[
-                            block(boardValues.won, 0, context),
-                            sizedBox(),
-                            block(boardValues.won, 1, context),
-                            sizedBox(),
-                            block(boardValues.won, 2, context),
-                          ],
-                        ),
+                  controller: parentClass),
+            )
+          : Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width / 1.2,
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height / 2,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          block(boardValues.won, 0, context),
+                          sizedBox(),
+                          block(boardValues.won, 1, context),
+                          sizedBox(),
+                          block(boardValues.won, 2, context),
+                        ],
                       ),
-                      Divider(
-                        color: Color(0xFF2C6171),
-                        height: 10,
-                        thickness: 5,
+                    ),
+                    Divider(
+                      color: Color(0xFF2C6171),
+                      height: 10,
+                      thickness: 5,
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          block(boardValues.won, 3, context),
+                          sizedBox(),
+                          block(boardValues.won, 4, context),
+                          sizedBox(),
+                          block(boardValues.won, 5, context),
+                        ],
                       ),
-                      Expanded(
-                        child: Row(
-                          children: <Widget>[
-                            block(boardValues.won, 3, context),
-                            sizedBox(),
-                            block(boardValues.won, 4, context),
-                            sizedBox(),
-                            block(boardValues.won, 5, context),
-                          ],
-                        ),
+                    ),
+                    Divider(
+                      color: Color(0xFF2C6171),
+                      height: 10,
+                      thickness: 5,
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: <Widget>[
+                          block(boardValues.won, 6, context),
+                          sizedBox(),
+                          block(boardValues.won, 7, context),
+                          sizedBox(),
+                          block(boardValues.won, 8, context),
+                        ],
                       ),
-                      Divider(
-                        color: Color(0xFF2C6171),
-                        height: 10,
-                        thickness: 5,
-                      ),
-                      Expanded(
-                        child: Row(
-                          children: <Widget>[
-                            block(boardValues.won, 6, context),
-                            sizedBox(),
-                            block(boardValues.won, 7, context),
-                            sizedBox(),
-                            block(boardValues.won, 8, context),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-      ),
+            ),
     );
   }
 
@@ -96,18 +95,32 @@ class Board extends StatelessWidget {
     if (win != 3) {
       print(win);
       boardValues.won = true;
+      if (win == 1) {
+        scoreBoardValues
+          ..increaseO()
+          ..increaseGame();
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => CupertinoAlertDialog(
+            title: Text('winner'),
+            content: Text('O'),
+          ),
+        );
+      } else {
+        scoreBoardValues
+          ..increaseX()
+          ..increaseGame();
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => CupertinoAlertDialog(
+            title: Text('winner'),
+            content: Text('X'),
+          ),
+        );
+      }
       Timer(
         Duration(seconds: 3),
         () {
-          if (win == 1) {
-            scoreBoardValues
-              ..increaseO()
-              ..increaseGame();
-          } else {
-            scoreBoardValues
-              ..increaseX()
-              ..increaseGame();
-          }
           boardValues.resetBoard();
           boardValues.won = false;
         },
@@ -122,14 +135,16 @@ class Board extends StatelessWidget {
         return Expanded(
           child: AnimatedContainer(
             duration: Duration(seconds: 3),
-            child: IconButton(
-              color: determineColorIcon(value.players, i),
-              iconSize: MediaQuery.of(context).size.width / 4,
-              icon: Icon(whichPlayer(i, value.players)),
-              onPressed: () {
-                value.setIcon(i);
-                winner();
-              },
+            child: Center(
+              child: IconButton(
+                color: determineColorIcon(value.players, i),
+                iconSize: MediaQuery.of(context).size.width / 5,
+                icon: Icon(whichPlayer(i, value.players)),
+                onPressed: () {
+                  value.setIcon(i);
+                  winner();
+                },
+              ),
             ),
           ),
         );
--- lib/ResetButton.dart
+++ lib/ResetButton.dart
@@ -7,44 +7,39 @@ import 'model/BoardValues.dart';
 class ResetButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Consumer<ScoreBoardValues>(
-      builder: (BuildContext context, ScoreBoardValues scoreBoardValues,
-          Widget child) {
-        return Consumer<BoardValues>(builder:
-            (BuildContext context, BoardValues boardValues, Widget child) {
-          return IconButton(
-            icon: Icon(Icons.autorenew),
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return CupertinoAlertDialog(
-                      title: Text('Reset'),
-                      content: Text('Are you sure?'),
-                      // backgroundColor: Color(0xFF2C6171),
-                      actions: <Widget>[
-                        CupertinoDialogAction(
-                          child: Text('Cancel'),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        CupertinoDialogAction(
-                          child: Text('ok'),
-                          onPressed: () {
-                            scoreBoardValues.resetScoreBoard();
-                            boardValues.resetBoard();
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  });
-            },
-            tooltip: 'reset',
-          );
-        });
+    var scoreBoardValues = Provider.of<ScoreBoardValues>(context);
+    var boardValues = Provider.of<BoardValues>(context);
+
+    return CupertinoButton(
+      child: Icon(Icons.autorenew),
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return CupertinoAlertDialog(
+                title: Text('Reset'),
+                content: Text('Are you sure?'),
+                // backgroundColor: Color(0xFF2C6171),
+                actions: <Widget>[
+                  CupertinoDialogAction(
+                    child: Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  CupertinoDialogAction(
+                    child: Text('ok'),
+                    onPressed: () {
+                      scoreBoardValues.resetScoreBoard();
+                      boardValues.resetBoard();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            });
       },
+      // tooltip: 'reset',
     );
   }
 }
